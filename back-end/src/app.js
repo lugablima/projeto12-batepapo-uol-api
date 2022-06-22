53afeca7e1e035393d503083d7f599605ae1b56f
LucasLima <lugablima@gmail.com>
2022-06-22 13:55:17 -0300

    chore: initialize the database and add environment variable

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,12 +1,20 @@
 import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 dotenv.config();
 
 const app = express();
 
 app.use([json(), cors()]);
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+mongoClient.connect().then(() => {
+  db = mongoClient.db("chatUOL");
+});
 
 app.listen(5000, () => {
   console.log("O Servidor está rodando em http://localhost:5000");
